65a84e6037f64615b29fa5443d49ed4c065a33b2
Hugo Gomes <hugo@sketch.com>
2020-06-05 01:02:19 +0100

    Using Rust

--- src/index.svelte
+++ src/index.svelte
@@ -1,10 +1,15 @@
 <script lang="typescript">
+// @ts-ignore
+    import module from './crate/Cargo.toml'
+
     const a: string = 'Svelte',
         b: string = 'Typescript',
-        c: string = 'Parcel'
+        c: string = 'Parcel',
+// @ts-ignore
+        d: string = module.run()
 </script>
 
-<h1>{a} + {b} + {c}</h1>
+<h1>{a} + {b} + {c} + {d}</h1>
 
 <style type="scss">
     h1 {
